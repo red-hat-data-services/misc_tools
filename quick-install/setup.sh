8f5e2cb4821c10e86ac849d6c35d1f54f75bc758
Chad Roberts <croberts@redhat.com>
2021-01-11 16:12:59 -0500

    Initial set of quick-install files

---
+++ quick-install/setup.sh
@@ -0,0 +1,8 @@
+echo "Make sure you are logged in as kubeadmin"
+
+oc new-project optest
+oc create -f service_account.yaml
+oc create -f role.yaml
+oc create -f cluster_role_binding.yaml
+oc create -f kfdef.apps.kubeflow.org_kfdefs_crd.yaml
+oc create -f operator.yaml
